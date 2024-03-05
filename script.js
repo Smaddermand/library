8ed595699aeead72f2e7c4a6acdc665e5dbe96ee
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-03-05 05:21:28 +0100

    Create form validation

--- script.js
+++ script.js
@@ -6,14 +6,19 @@ const newBookForm = document.getElementById("newbook-form");
 
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return(title + " by " + author + ", " + pages + " pages, " + read)};
-};
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.info = function(){
+            return(title + " by " + author + ", " + pages + " pages, " + read)};    
+        
+    }
+}
+
+
 
 function addBookToLibrary(title, author, pages, read){
     let newBook = new Book(title, author, pages, read);
